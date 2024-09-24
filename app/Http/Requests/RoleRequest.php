770de46a056f75d724e40b8f7fca22cc52d97e15
Nguyễn Thành Đạt <dat03021999@gmail.com>
2024-09-24 10:57:49 +0700

    Update RoleRequest.php

--- app/Http/Requests/RoleRequest.php
+++ app/Http/Requests/RoleRequest.php
@@ -11,7 +11,7 @@ class RoleRequest extends FormRequest
         return [
             'name' => ['required'],
             'description' => ['nullable'],
-            'policies' => ['required', 'array']
+            'policies' => ['array']
         ];
     }
 
